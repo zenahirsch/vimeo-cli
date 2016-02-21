5cfcd7c79ec21c2a5bd6bb5927b18485f3174936
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-21 16:51:22 -0500

    remove unnecessary if statements

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ program
     .command('use <nickname>')
     .description('Use the specified user to authenticate requests.')
     .action (function (nickname) {
-        if (nickname) {
-            lib.getScript('use')(nickname);
-        }
+        lib.getScript('use')(nickname);
     });
 
 program
@@ -42,16 +40,12 @@ program
     .command('remove <nickname>')
     .description('Removes specified user from the utility.')
     .action(function (nickname) {
-        if (nickname) {
-            lib.getScript('remove')(nickname);
-        }
+        lib.getScript('remove')(nickname);
     });
 
 program
     .command('upload <path>')
-    .description('Upload a video or directory of videos.')
-    .option('-f, --file', 'Path to the specified file')
-    .option('-d, --directory', 'Path to the specified directory')
+    .description('Upload a video.')
     .action(function (path) {
         lib.getScript('upload')(path);
     });
