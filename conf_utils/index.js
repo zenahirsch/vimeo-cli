79685cf8b614ec9b5c0311bb8c12051f48ad9757
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-21 15:41:21 -0500

    a whole bunch of stuff to get started

---
+++ conf_utils/index.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var nconf = require('nconf');
+var client_conf = require(__dirname + '/../client_conf.json');
+
+module.exports = {
+    setup: function () {
+        nconf.file({ 
+            file: __dirname + '/../config.json',
+            secure: {
+                secret: 'super-secretzzz-keyzz',
+                alg: 'aes-256-ctr'
+            }
+        });
+
+        nconf.defaults({
+            client_id: client_conf.client_id,
+            client_secret: client_conf.client_secret,
+            users: []
+        });
+
+        return this;
+    },
+    save: function () {
+        nconf.save(function (err) {
+            if (err) throw err;
+        });
+    },
+    set: function (key, value) {
+        nconf.set(key, value);
+
+        return this;
+    },
+    get: function (key) {
+        var value = null;
+
+        if (nconf.get(key)) {
+            value = nconf.get(key);
+        }
+
+        return value;
+    }
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-var pkg = require(__dirname + '/package.json');
+var pkg = require(__dirname + '/package');
 var lib = require(__dirname + '/lib');
 
 program
@@ -9,12 +9,51 @@ program
     .description('A command line interface for Vimeo.');
 
 program
-    .command('setup')
-    .description('Connect to your Vimeo account.')
+    .command('setup <client_id> <client_secret>')
+    .description('Setup utility. Do this before running `vimeo auth`')
+    .action(function (client_id, client_secret) {
+        lib.getScript('setup')(client_id, client_secret)
+    });
+
+program
+    .command('auth')
+    .description('Authenticate a new Vimeo account.')
     .action (function () {
-        lib.setup();
+        lib.getScript('auth')();
+    });
+
+program
+    .command('use <nickname>')
+    .description('Use the specified user to authenticate requests.')
+    .action (function (nickname) {
+        if (nickname) {
+            lib.getScript('use')(nickname);
+        }
+    });
+
+program
+    .command('users')
+    .description('List all authenticated users.')
+    .action(function () {
+        lib.getScript('users')();
+    });
+
+program
+    .command('remove <nickname>')
+    .description('Removes specified user from the utility.')
+    .action(function (nickname) {
+        if (nickname) {
+            lib.getScript('remove')(nickname);
+        }
+    })
+
+program
+    .command('upload')
+    .description('Upload a video or directory of videos.')
+    .option('-f, --file <path>', 'Path to the specified file')
+    .option('-d, --directory <path>', 'Path to the specified directory')
+    .action(function (options) {
+        lib.getScript('upload')(options);
     });
 
 program.parse(process.argv);
-
-program.help();
