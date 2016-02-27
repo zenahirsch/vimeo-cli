d9505650d2f1ab7920a3f056a83b32c4769779eb
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-27 12:42:06 -0500

    add 'add' commands and aliases

--- index.js
+++ index.js
@@ -51,10 +51,62 @@ program
     });
 
 program
-    .command('list <item>')
+    .command('list <item_name>')
     .description('List the specified items (videos, albums, followers, etc.)')
-    .action((item) => {
-        lib.getScript('list')(item)
+    .action((item_name) => {
+        lib.getScript('list')(item_name);
     });
+
+program
+    .command('add <item_name>')
+    .description('Add a new item (album, group, channel, etc.)')
+    .option('-n, --name <name>', 'Specify a name')
+    .option('-d, --description <description>', 'Specify a description')
+    .option('-p, --privacy <privacy>', 'Specify a privacy')
+    .option('-P, --password <password>', 'Specify a password')
+    .option('-s, --sort <sort>', 'Specify a sort')
+    .option('-u, --user <user_id>', 'Specify a user ID')
+    .option('-v, --video <video_id>', 'Specify a video ID')
+    .action((item_name, options) => {
+        lib.getScript('add')(item_name, options);
+    });
+
+
+// Alias for `add user -u <user_id>`
+program
+    .command('follow <user_id>')
+    .description('Follow the specified user.')
+    .action((user_id) => {
+        var options = {
+            user: user_id
+        };
+
+        lib.getScript('add')('following', options);
+    });
+
+// Alias for `add like -v <video_id>`
+program
+    .command('like <video_id>')
+    .description('Like the specified video.')
+    .action((video_id) => {
+        var options = {
+            video: video_id
+        };
+
+        lib.getScript('add')('like', options);
+    });
+
+// Alias for `add watchlater -v <video_id>`
+program
+    .command('watchlater <video_id>')
+    .description('Add the specified video to your watch later queue.')
+    .action((video_id) => {
+        var options = {
+            video: video_id
+        };
+
+        lib.getScript('add')('watchlater', options);
+    });
+
 
 program.parse(process.argv);
